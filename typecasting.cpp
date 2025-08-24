32f0e1f4f4f56ea765c5ab72b8f9e3f08dfc9c31
Skywolf02 <henilchaudhari4422@gmail.com>
2025-08-24 18:05:10 +0530

    day 3 end

--- typecasting.cpp
+++ typecasting.cpp
@@ -5,6 +5,7 @@ int main(){
     //cout<<(10/3.0)<<endl;
     //cout<<((int)'b'+1)<<endl;
     cout<<( (float)10/3)<<endl;
+    cout<<(23.5+2+'A');
     
     return 0;
 
---
+++ unary.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 3;
+    a++;
+    cout<<"a="<<a<<endl;
+    a--;
+    cout<<"a="<<a<<endl;
+    // for ++a and --a (pre)then it will update & use 
+    return 0;
+}
